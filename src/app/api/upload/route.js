5f43a26e7ad749d6f093093c4290fe2ef8ac6019
Ivan Martinez <pr4zka@Ivans-MacBook-Pro.local>
2024-04-25 19:03:52 -0400

    version final for production

--- src/app/api/upload/route.js
+++ src/app/api/upload/route.js
@@ -1,30 +1,35 @@
 import { NextResponse } from 'next/server';
 import axios from 'axios';
 
-export async function POST(request) {
-  const data = await request.formData();
-  const file = data.get('file');
-  const pdf = !file;
-  if (pdf) return NextResponse.json({ message: 'File not found' }, { status: 400 });
+export function POST(request) {
+  return new Promise((resolve, reject) => {
+    request.formData().then(async (data) => {
+      const file = data.get('file');
+      const pdf = !file;
+      if (pdf) {
+        resolve(NextResponse.json({ message: 'File not found' }, { status: 400 }));
+      }
 
-  const formData = new FormData();
-  formData.append('file', file);
+      const formData = new FormData();
+      formData.append('file', file);
 
-  try {
-    const pdf_template = await axios.post(process.env.BACKEND_URL, formData, {
-      responseType: 'arraybuffer',
+      try {
+        const pdf_template = await axios.post(process.env.BACKEND_URL, formData, {
+          responseType: 'arraybuffer',
+        });
+
+        const response = new NextResponse(pdf_template.data, {
+          headers: {
+            'Content-Type': 'application/pdf',
+            'Content-Disposition': 'attachment; filename="plantilla-factura.pdf"',
+          },
+        });
+
+        resolve(response);
+      } catch (error) {
+        console.error('Error al obtener el PDF:', error);
+        resolve(NextResponse.json({ message: 'Error al obtener el PDF' }, { status: 500 }));
+      }
     });
-
-    const response = new NextResponse(pdf_template.data, {
-      headers: {
-        'Content-Type': 'application/pdf',
-        'Content-Disposition': 'attachment; filename="plantilla-factura.pdf"',
-      },
-    });
-
-    return response;
-  } catch (error) {
-    console.error('Error al obtener el PDF:', error);
-    return NextResponse.json({ message: 'Error al obtener el PDF' }, { status: 500 });
-  }
+  });
 }
